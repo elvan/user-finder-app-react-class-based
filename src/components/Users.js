70ec4c2f3e160756054668a548f31e58cf875f41
Elvan H <elvanhd@gmail.com>
2021-12-06 22:53:31 +0700

    lifecycle methods example

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,43 +2,37 @@ import { Component } from 'react';
 import User from './User';
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' }
-];
-
 class Users extends Component {
-  constructor() {
-    super([]);
+  constructor(props) {
+    super(props);
     this.state = {
-      showUsers: false
+      showUsers: true,
+      more: 'Test'
     };
   }
 
-  toggleUsersHandler = () => {
-    this.setState((state) => {
-      return {
-        showUsers: !state.showUsers
-      };
+  toggleUsersHandler() {
+    // this.state.showUsers = false; // NOT!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
     });
-  };
-
-  usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  }
 
   render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
     return (
       <div className={classes.users}>
-        <button onClick={this.toggleUsersHandler}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
           {this.state.showUsers ? 'Hide' : 'Show'} Users
         </button>
-        {this.state.showUsers && this.usersList}
+        {this.state.showUsers && usersList}
       </div>
     );
   }
