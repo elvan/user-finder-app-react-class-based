45ad242e6dafc903c2723cc3c961fd1d038ea2cf
Elvan Hidayat <elvanhd@gmail.com>
2024-06-14 22:56:56 +0700

    Add ErrorBoundary to handle errors in UserFinder
    - Create ErrorBoundary component for error handling
    - Implement componentDidCatch to log errors and update state
    - Wrap Users component with ErrorBoundary in UserFinder
    - Add error throwing in Users component for empty user list

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,6 +1,7 @@
 import { Component, Fragment } from 'react';
 
 import UsersContext from '../store/users-context';
+import ErrorBoundary from './ErrorBoundary';
 import classes from './UserFinder.module.css';
 import Users from './Users';
 
@@ -40,7 +41,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type='search' onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
